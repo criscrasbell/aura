c2a79c298a00e9835f5a39c2fcfd5940fa8c7e24
Chris Bell <chrisclarkbell95@gmail.com>
2022-11-06 12:13:53 -0700

    feat: profile react component

---
+++ components/profile.tsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+    return (
+      <div className="flex flex-col items-center justify-center mb-4">
+        <img className="rounded-full h-36 w-36" src="/profile.png" />
+        <h1 className="prose prose-2xl font-black">Christopher Bell</h1>
+        <h1 className="prose prose-sm thin">Software Engineer</h1>
+      </div>
+    );
+  };
+  
+  export default Profile;
